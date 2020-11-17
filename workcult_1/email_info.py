7ea9fcf4840bf00084827a76a641318e9cebe4ae
Anup Doddamani <anup2000ster@gmail.com>
2020-11-17 16:54:33 +0530

    pip updated and email_info used

---
+++ workcult_1/email_info.py
@@ -0,0 +1,6 @@
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USERNAME = 'anup95dev@gmail.com'
+EMAIL_HOST_PASSWORD = 'oidmcjtuchljcogc'
+EMAIL_PORT = 587
